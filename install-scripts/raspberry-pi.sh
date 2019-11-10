a224e34fcacf0454232d4be01af2a6afaf89b7d4
Tycho Bokdam <tycho@palmtreecoding.com>
2019-11-10 15:15:08 +0100

    chore: Added non tested PI script

--- install-scripts/raspberry-pi.sh
+++ install-scripts/raspberry-pi.sh
@@ -1,4 +1,36 @@
 #!/bin/sh
 set -e
 
-echo "This script is not yet available"
+echo "Installing essential tools..."
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+sudo apt-get update
+
+sudo apt-get install -y \
+  nodejs \
+  git \
+  mongodb-org
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update && sudo apt-get install yarn
+
+echo "Create directory..."
+sudo mkdir -p /pct-org
+
+sudo chmod -R 7777 /pct-org
+
+cd /pct-org
+
+# Remove getting started to be sure
+rm -rf ./getting-started
+
+echo "Cloning repo"
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+git clone --recurse-submodules https://github.com/pct-org/getting-started.git
+
+cd getting-started
+yarn install
+yarn setup
