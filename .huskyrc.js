4451b0af08389b1ee0fcb598e544aaceed3d798c
Tycho Bokdam <tycho@palmtreecoding.com>
2020-04-26 16:51:16 +0200

    fix: Update graphql-api to v0.6.2

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,7 +1,5 @@
 module.exports = {
   hooks: {
-    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-    'post-checkout': 'yarn build:apis',
-    'post-merge': 'yarn build:apis',
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS'
   },
 }
